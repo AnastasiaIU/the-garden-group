f3dc8ca05749f27bf70e1b3a991980ef835781d1
Orest <opokotylenkowork@gmail.com>
2024-10-01 01:46:23 +0200

    basic rest api

---
+++ GardenGroupAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using DAL;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace GardenGroupAPI.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeDao _employeeDao;
+
+        public EmployeeController(EmployeeDao employeeDao)
+        {
+            _employeeDao = employeeDao;
+        }
+
+        [HttpGet]
+        public async Task<List<Employee>> Get()
+        {
+            return await _employeeDao.GetAllEmployeesAsync();
+        }
+    }
+}
---
+++ GardenGroupAPI/Controllers/TicketController.cs
@@ -0,0 +1,24 @@
+﻿using DAL;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace GardenGroupAPI.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+    public class TicketController : Controller
+    {
+        private readonly TicketDao _ticketDao;
+
+        public TicketController(TicketDao ticketDao)
+        {
+            _ticketDao = ticketDao;
+        }
+
+        [HttpGet]
+        public async Task<List<Ticket>> Get()
+        {
+            return await _ticketDao.GetAllTicketsAsync();
+        }
+    }
+}
