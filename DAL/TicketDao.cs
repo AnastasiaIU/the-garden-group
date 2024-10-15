3d7952c3c2a95f2f7caffe9c1f666d01f5812d46
Vhalenko <galenko.vladislav@gmail.com>
2024-10-15 12:34:16 +0200

    dashboard crud

--- DAL/TicketDao.cs
+++ DAL/TicketDao.cs
@@ -96,5 +96,55 @@ namespace DAL
             var update = Builders<Ticket>.Update.Set("is_escalated", true);
             await ticketCollection.UpdateOneAsync(filter, update);
         }
+
+        //Vladyslav
+        //Count tickets
+        public async Task<int> CountResolvedTicketsForReportingUserAsync(string employeeId)
+        {
+            var filter = Builders<Ticket>.Filter.Eq(t => t.ReportingUser, employeeId)
+                & Builders<Ticket>.Filter.Eq(t => t.IsResolved, true)
+                & Builders<Ticket>.Filter.Eq(t => t.Status, Status.Closed);
+            return (int)await ticketCollection.CountDocumentsAsync(filter);
+        }
+
+        public async Task<int> CountResolvedTicketsForServiceDeskUserAsync(string employeeId)
+        {
+            var filter = Builders<Ticket>.Filter.Eq(t => t.ServiceDeskUser, employeeId)
+                & Builders<Ticket>.Filter.Eq(t => t.IsResolved, true)
+                & Builders<Ticket>.Filter.Eq(t => t.Status, Status.Closed);
+            return (int)await ticketCollection.CountDocumentsAsync(filter);
+        }
+
+        public async Task<int> CountOpenTicketsForReportingUserAsync(string employeeId)
+        {
+            var filter = Builders<Ticket>.Filter.Eq(t => t.ReportingUser, employeeId)
+                & Builders<Ticket>.Filter.Eq(t => t.IsResolved, false)
+                & Builders<Ticket>.Filter.Eq(t => t.Status, Status.Open);
+            return (int)await ticketCollection.CountDocumentsAsync(filter);
+        }
+
+        public async Task<int> CountOpenTicketsForServiceDeskUserAsync(string employeeId)
+        {
+            var filter = Builders<Ticket>.Filter.Eq(t => t.ServiceDeskUser, employeeId)
+                & Builders<Ticket>.Filter.Eq(t => t.IsResolved, false)
+                & Builders<Ticket>.Filter.Eq(t => t.Status, Status.Open);
+            return (int)await ticketCollection.CountDocumentsAsync(filter);
+        }
+
+        public async Task<int> CountClosedTicketsForReportingUserAsync(string employeeId)
+        {
+            var filter = Builders<Ticket>.Filter.Eq(t => t.ReportingUser, employeeId)
+                & Builders<Ticket>.Filter.Eq(t => t.IsResolved, false)
+                & Builders<Ticket>.Filter.Eq(t => t.Status, Status.Closed);
+            return (int)await ticketCollection.CountDocumentsAsync(filter);
+        }
+
+        public async Task<int> CountClosedTicketsForServiceDeskUserAsync(string employeeId)
+        {
+            var filter = Builders<Ticket>.Filter.Eq(t => t.ServiceDeskUser, employeeId)
+                & Builders<Ticket>.Filter.Eq(t => t.IsResolved, false)
+                & Builders<Ticket>.Filter.Eq(t => t.Status, Status.Closed);
+            return (int)await ticketCollection.CountDocumentsAsync(filter);
+        }
     }
 }
--- Service/TicketService.cs
+++ Service/TicketService.cs
@@ -27,5 +27,37 @@ namespace Service
         {
             await ticketDao.EscalateTicket(ticketId);
         }
+
+        //Vladyslav
+        //Count tickets
+        public async Task<int> CountResolvedTicketsForReportingUserAsync(string employeeId)
+        {
+            return await ticketDao.CountResolvedTicketsForReportingUserAsync(employeeId);
+        }
+
+        public async Task<int> CountResolvedTicketsForServiceDeskUserAsync(string employeeId)
+        {
+            return await ticketDao.CountResolvedTicketsForServiceDeskUserAsync(employeeId);
+        }
+
+        public async Task<int> CountOpenTicketsForReportingUserAsync(string employeeId)
+        {
+            return await ticketDao.CountOpenTicketsForReportingUserAsync(employeeId);
+        }
+
+        public async Task<int> CountOpenTicketsForServiceDeskUserAsync(string employeeId)
+        {
+            return await ticketDao.CountOpenTicketsForServiceDeskUserAsync(employeeId);
+        }
+
+        public async Task<int> CountClosedTicketsForReportingUserAsync(string employeeId)
+        {
+            return await ticketDao.CountClosedTicketsForReportingUserAsync(employeeId);
+        }
+
+        public async Task<int> CountClosedTicketsForServiceDeskUserAsync(string employeeId)
+        {
+            return await ticketDao.CountClosedTicketsForServiceDeskUserAsync(employeeId);
+        }
     }
 }
